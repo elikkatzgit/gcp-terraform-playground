2b480b521dcdc9e36a52c7b367968279b108b385
elikkatzgit <50174655+elikkatzgit@users.noreply.github.com>
2023-08-16 12:08:24 +0300

    Update instance_setup.sh

--- instance_setup.sh
+++ instance_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo useradd hadmin
-echo "hadmin:Harman@Azure" | chpasswd
+sudo useradd XXXX
+echo "XXXX" | chpasswd
 echo "hadmin ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 systemctl restart sshd
